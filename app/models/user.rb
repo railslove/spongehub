47d10cb80c4eec74181bd47f5de3b329dc46f4ab
Jan Kus <jan@railslove.com>
2012-10-13 19:30:44 +0200

    added relations user - chore - fames and blames

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
   attr_accessible :name
 
+  has_many :chores, dependent: :destroy, foreign_key: "taker_id"
+  has_many :ratings, dependent: :destroy
+  has_many :fames,  class_name: "Rating", foreign_key: "rated_id", conditions: "value > 0"
+  has_many :blames, class_name: "Rating", foreign_key: "rated_id", conditions: "value < 0"
+
   has_and_belongs_to_many :spaces
 end
