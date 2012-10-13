bab2054032e0093eef0ccb340c406b2f291c54f2
Jan Kus <jan@railslove.com>
2012-10-13 21:35:57 +0200

    added new default picture for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  include Sprockets::Helpers::RailsHelper
+  include Sprockets::Helpers::IsolatedHelper
+
   attr_accessible :name
 
   has_many :chores, dependent: :destroy, foreign_key: "taker_id"
@@ -7,4 +10,9 @@ class User < ActiveRecord::Base
   has_many :blames, class_name: "Rating", foreign_key: "rated_id", conditions: "value < 0"
 
   has_and_belongs_to_many :spaces
+
+  def remote_image_url
+    read_attribute(:remote_image_url).presence || asset_path("fallbacks/default_remote_image.png")
+  end
+
 end
