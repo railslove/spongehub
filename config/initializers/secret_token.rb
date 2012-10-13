77c465ae049853634d325eb92e4d28aa482f8df3
Jakob Hilden <jakobhilden@gmail.com>
2012-10-13 09:28:29 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rumble12::Application.config.secret_token = 'e8acef8de8b078dac6bd351a15d4b63a615a3570e857815ab2e860347361cfa800b321c95797f0c09782573655093564df264685cc0d4dfa5111ff29aa50f1c8'
