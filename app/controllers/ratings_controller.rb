ca6c29b8b599c6c54ab12a6413c601924856f8b7
Jan Kus <jan@railslove.com>
2012-10-14 13:11:52 +0200

    removed all rating views and changed redirects while creating a rating

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -3,37 +3,6 @@ class RatingsController < ApplicationController
   before_filter :authentication_required
   before_filter :load_and_ensure_space
 
-  def index
-    @ratings = @space.ratings.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @ratings }
-    end
-  end
-
-  def show
-    @rating = @space.ratings.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @rating }
-    end
-  end
-
-  def new
-    @rating = @space.ratings.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @rating }
-    end
-  end
-
-  def edit
-    @rating = @space.ratings.find(params[:id])
-  end
-
   def create
     @rating = @space.ratings.new(params[:rating], :creator => current_user)
 
@@ -42,21 +11,7 @@ class RatingsController < ApplicationController
         format.html { redirect_to @space, notice: 'Rating was successfully created.' }
         format.json { render json: @rating, status: :created, location: [@space, @rating] }
       else
-        format.html { render action: "new" }
-        format.json { render json: @rating.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def update
-    @rating = @space.ratings.find(params[:id])
-
-    respond_to do |format|
-      if @rating.update_attributes(params[:rating])
-        format.html { redirect_to @rating, notice: 'Rating was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
+        format.html { redirect_to @space, alert: 'Something went wrong. Maybe you forgot the descritpion?' }
         format.json { render json: @rating.errors, status: :unprocessable_entity }
       end
     end
